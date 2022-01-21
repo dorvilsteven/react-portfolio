15beaad09858ec9e78e51e8b80907be5408853c2
StevenD <dorvilsteven@gmail.com>
2022-01-20 20:40:24 -0500

    updated with new project

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -5,28 +5,29 @@ import mainWork from '../../components/pictures/kitchensinkchef.png';
 const Portfolio = () => {
     return (
         <section id="portfolio">
-            <section class="portfolio-title">
+            <section className="portfolio-title">
                 <h2>Portfolio Works</h2>
             </section>
-            <section class="portfolio-works">
-                <article class="active-work">
-                <div class="active-work-image">
+            <section className="portfolio-works">
+                <article className="active-work">
+                <div className="active-work-image">
                     <a href="https://dorvilsteven.github.io/kitchen-sink-chef/" target="_blank">
                     <img src={mainWork} alt="Kitchen sink chef image" />
                     </a>
                 </div>
-                <div class="active-work-text">
+                <div className="active-work-text">
                     <h4>Most Recent Work:</h4>
-                    <span class="work-title">Kitchen Sink Chef</span>
+                    <span className="work-title">Kitchen Sink Chef</span>
                 </div>
                 </article>
-                <section class="side-works">
-                <a href="http://smagums.herokuapp.com/" class="work smagums" target="_blank"></a>
-                <a href="https://dorvilsteven.github.io/run-buddy/" class="work run-buddy" target="_blank"></a>
-                <a href="https://dorvilsteven.github.io/weather-app/" class="work weather-app" target="_blank"></a>
-                <a href="https://dorvilsteven.github.io/work-day-scheduler/" class="work workday" target="_blank"></a>
-                <a href="https://dorvilsteven.github.io/javascript-quiz-app/" class="work quiz" target="_blank"></a>
-                <a href="https://tech-blog-mvc-steven-dorvil.herokuapp.com/" class="work tech-blog" target="_blank"></a>
+                <section className="side-works">
+                    <a href="https://shmovies.herokuapp.com/" className="work shmovies" target="_blank"></a>
+                    <a href="http://smagums.herokuapp.com/" className="work smagums" target="_blank"></a>
+                    <a href="https://dorvilsteven.github.io/run-buddy/" className="work run-buddy" target="_blank"></a>
+                    <a href="https://dorvilsteven.github.io/weather-app/" className="work weather-app" target="_blank"></a>
+                    <a href="https://dorvilsteven.github.io/work-day-scheduler/" className="work workday" target="_blank"></a>
+                    <a href="https://dorvilsteven.github.io/javascript-quiz-app/" className="work quiz" target="_blank"></a>
+                    <a href="https://tech-blog-mvc-steven-dorvil.herokuapp.com/" className="work tech-blog" target="_blank"></a>
                 </section>
             </section>
         </section>
